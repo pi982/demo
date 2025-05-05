2892175dd5d5c4469216416ac2071ddf861bd1b5
pi982 <piday1432025@gmail.com>
2025-05-05 21:52:38 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1232,6 +1232,7 @@ document.addEventListener("DOMContentLoaded", function () {
         let html = `
         <html>
           <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <title>Báo cáo điểm danh${!hasMultipleClasses ? " - " + headerClassText : ""}</title>
             <style>
               body {
